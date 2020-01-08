a74e071e910de51e45daef43bd8ad89254cd170b
crissesoto <soto.crisse@hotmail.com>
2020-01-08 14:50:49 +0100

    added app.post metho

--- app.js
+++ app.js
@@ -5,15 +5,20 @@ const port = 3000;
 
 const app = expresss();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expresss.static('public'));
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/signup.html');
 });
 
 app.post('/', function(req, res) {
-	console.log(req);
+	console.log(req.body);
+	const data = req.body;
+	const firstName = data.firstName;
+	const lastName = data.lastName;
+	const email = data.email;
 
-	res.send('file sent!');
+	res.send(`Hello ${firstName} ${lastName}, your are registrated with ${email}}`);
 });
 
 app.listen(port, function() {
