919f6e25389ae2a99e7c69b7012aea2adc36f497
crissesoto <soto.crisse@hotmail.com>
2020-01-08 12:37:51 +0100

    testing shh key

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = expresss();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/signup.html');
 });
 
 app.post('/', function(req, res) {
